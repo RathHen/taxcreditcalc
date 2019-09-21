2dc0c0c1b7f59e8d0ed37839157ddc5fba3d029c
gjb2054 <gjb2054@rit.edu>
2019-09-21 11:27:57 -0400

    Updates

--- taxcreditcalc/src/Components/resultant.js
+++ taxcreditcalc/src/Components/resultant.js
@@ -6,16 +6,31 @@ import { PARENT_MESSAGE_CLIENT_ERROR } from 'jest-worker/build/types';
 export class resultants extends Component {
     state = {
         num: 0,
+        calculated: 0,
       };
-
+      calculate = (wage,rent) => {
+        var ratio = ParseFloat(wage)/parseInt(rent);
+        if(ratio>1.0/3){
+          return ratio*parseFloat(wage) - parseFloat(wage)*(1.0/3);
+        }
+    };
       result = (wage, rent) => {
-          this.setState({num: parseInt(wage)+parseInt(rent)});
+          this.setState({num: this.calculate(wage,rent)});
       };
+    
+
       
 
       
     
       render() {
+        if(this.state.num != 0) {
+            return (
+                <div>
+                  <h3>{this.state.num}</h3>
+                </div>
+            )
+        }
         return (
           
           <div>
