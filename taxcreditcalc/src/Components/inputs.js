1e4ea72ad28f17340706585593fff04f4749305b
42045764 <rathhen123@gmail.com>
2019-09-21 00:42:19 -0400

    component folder

---
+++ taxcreditcalc/src/Components/inputs.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class inputs extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default inputs
+
