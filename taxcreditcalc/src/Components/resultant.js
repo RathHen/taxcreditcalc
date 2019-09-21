9aa12ba468952e9791fb50838cfa0211c1f63ad8
gjb2054 <gjb2054@rit.edu>
2019-09-21 01:47:26 -0400

    Resultant File

---
+++ taxcreditcalc/src/Components/resultant.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class resultants extends Component {
+
+    render() {
+        return (
+                <div className="ui input">
+                    <input type="number" placeholder="Enter Monthly Wage"/> 
+                    <input type="number" placeholder="Enter Monthly Rent"/> 
+                </div>
+            
+        )
+    }
+}
+
+export default reusltants
