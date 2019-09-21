2c0e5c86b0b1724a030dd164dfe1393b1a4634d1
gjb2054 <gjb2054@rit.edu>
2019-09-21 01:43:43 -0400

    Form For Finanaces

--- taxcreditcalc/src/Components/finanaceForm.js
+++ taxcreditcalc/src/Components/finanaceForm.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react'
 
 export class finanaceForm extends Component {
+    state = {
+        monthlyWage: 0,
+        monthlyRent: 0
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.calculate(this.state.monthlyWage, this.state.monthlyRent)
+    }
 
     render() {
         return (
-            <form class="ui form">
+         <form class="ui form" onSubmit={this.onSubmit}>
             <div class="field">
               <div class="six wide field">
                 <label>Monthly Wage</label>
-                <input type="number" name="wage" placeholder="Monthly Wage"/>
+                <input type="number" name="wage" placeholder="Monthly Wage" value="this.state.monthlyWage"/>
               </div>
             </div>
             <div class="field">
               <div class="six wide field">
                 <label>Mothly Rent</label>
-                <input type="number" name="rent" placeholder="Monthly Rent"/>
+                <input type="number" name="rent" placeholder="Monthly Rent" value="this.state.monthlyRent"/>
               </div>  
             </div>
             <button class="ui button" type="submit">Submit</button>
