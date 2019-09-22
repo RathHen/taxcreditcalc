6d738d3abd75b242ee21778881f142e0aad1b1b2
Zilu Wang <ziluwang@dhcp-ccc-7628.eduroam.cornell.edu>
2019-09-22 01:12:04 -0400

    formatted

--- taxcreditcalc/src/Components/finanaceForm.js
+++ taxcreditcalc/src/Components/finanaceForm.js
@@ -41,55 +41,57 @@ export class finanaceForm extends Component {
     render() {
         return (
           <React.Fragment>  
+
             <font face="Times New Roman">
 <br></br>
 <center><h1>
   Housing Subsidy Calculator
+  <br></br>
   </h1></center>
-
-            <h2> The issue:</h2>
-<p>The housing inequality crisis refers to the increasing disparity in the quality of housing in the U.S. Quality of housing includes multiple aspects, including crime rate, quality of local schools, the amenities and the amount of air pollution.</p>
-
-<h2>  The solution:</h2>
-<p>  <font size="+2"> Our team believes that an effective solution to this issue is subsidized 
+            <h2 style={{marginLeft: "5%",marginRight: "20%"}}> The issue:</h2>
+<p style={{marginLeft: "5%",marginRight: "30%"}}><font size="+1">The housing inequality crisis refers to the increasing disparity in the quality of housing in the U.S. Quality of housing includes multiple aspects, including crime rate, quality of
+ local schools, the amenities and the amount of air pollution.</font></p>
+<h2 style={{marginLeft: "5%",marginRight: "30%"}}>  The solution:</h2>
+<p style={{marginLeft: "5%",marginRight: "30%"}}>  <font size="+1"> Our team believes that an effective solution to this issue is subsidized 
 housing. This solution involves the government providing rent supplements to impoverished households.
  To avoid potential misuses of state funding, the rent supplements should be given directly to landlord
  s. For instance, a person with an extensive history of drug abuse might spend rent supplements on dru
  gs, and giving rent supplements directly to the landlord prevents this from occurring.</font> </p>
- 
-<h2>  How the housing subsidy calculator contributes to the solution:</h2>
-<p>   A key challenge in providing subsidies is determining the correct amount of subsidy to allocate to each household. To address this challenge, our team developed a housing subsidy calculator, which allows individuals to check the amount of subsidy that they should receive to guarantee a reasonable level of housing quality. </p>
 
-<h2>How the housing subsidy calculator works:</h2>
-<p>The calculator takes five inputs from the user:
+<h2 style={{marginLeft: "5%",marginRight: "30%"}}>  How the housing subsidy calculator contributes to the solution:</h2>
+<p style={{marginLeft: "5%",marginRight: "30%"}}>  <font size="+1">  A key challenge in providing subsidies is determining the correct amount of subsidy to allocate to each household. To address this challenge, our team developed a housing subsidy calculator, which allows individuals to check the amount of subsidy that th
+ey should receive to guarantee a reasonable level of housing quality.</font> </p>
+
+<h2 style={{marginLeft: "5%",marginRight: "30%"}}>How the housing subsidy calculator works:</h2>
+<p style={{marginLeft: "5%",marginRight: "30%"}}>  <font size="+1">The calculator takes five inputs from the user:
   <ol>
 <li>Monthly wage</li>
 <li>Monthly rent</li>
 <li>Zip code</li>
 <li>Number of dependents</li>
 <li>Marital status</li></ol>
-Using the wage and rent, the calculator computes a ratio that represents the proportion of an individual’s income that’s spent on housing. This ratio is then compared with the “ideal” ratio of 1/3, and the difference between these two ratios suggests the amount of subsidy that’s required. 
+Using the wage and rent, the calculator computes a ratio that represents the proportion of an individual’s income that’s spent on housing. This ratio is then compared with the “ideal” ratio of 1/3, and the difference between these two ratios suggests the amount of subsidy that’s required. </font>
 </p>
 
-
-<center><h1>Instructions:</h1></center>
-<p><ol>
-<li>Type in your pre-tax monthly wage in the first input box below </li>
-[note: your monthly wage must be below $4,000 to qualify for housing subsidy]
-
-<li>Type in your monthly rent in the second box</li>
-
-<li>Type in your zip code; this will be used to factor the crime rate and mental health index into our calculation</li>
-
-<li>Type in the number of your dependents, including the number of your children and parents that you need to provide financial support for</li>
-
-<li>Select your marital status, click on the “submit” button if you’re unmarried</li>
-
-<li>If you’re married or divorced, enter the income of or financial support that you receive from the other person</li>
-</ol>
-See the formula that this calculator uses here.
+<br></br><br></br>
+<center><h1>Instructions:</h1></center><br></br>
+<p style={{marginLeft: "5%",marginRight: "30%"}}>  <font size="+1"><ol>
+<li>Type in your pre-tax monthly wage in the first input box below. </li>
+[note: your monthly wage must be below $4,000 to qualify for housing subsidy]<br></br>
+<br></br>
+<li>Type in your monthly rent in the second box.</li>
+<br></br>
+<li>Type in your zip code; this will be used to factor the crime rate and mental health index into our calculation.</li>
+<br></br>
+<li>Type in the number of your dependents, including the number of your children and parents that you need to provide financial support for.</li>
+<br></br>
+<li>Select your marital status, click on the “submit” button if you’re unmarried.</li>
+<br></br>
+<li>If you’re married or divorced, enter the income of or financial support that you receive from the other person.</li>
+</ol><br></br>
+See the formula that this calculator uses here.</font>
 </p></font>
-
+<br></br><br></br><br></br>
 
             <form className="ui form" onSubmit={this.onSubmit} style={this.getFormStyle()}>
             <h1 style={{color: "white", marginLeft: "23%"}}>The Housing Subsidy Calculator</h1>
