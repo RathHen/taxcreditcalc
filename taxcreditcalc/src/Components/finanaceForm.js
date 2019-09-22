ccccbe2800994c5d77c4aa4c69e3811fac79fe06
Zilu Wang <ziluwang@dhcp-ccc-7628.eduroam.cornell.edu>
2019-09-21 20:14:18 -0400

    Reformatted

--- taxcreditcalc/src/Components/finanaceForm.js
+++ taxcreditcalc/src/Components/finanaceForm.js
@@ -30,7 +30,46 @@ export class finanaceForm extends Component {
 
     render() {
         return (
-          <React.Fragment>    
+          <React.Fragment>  
+
+            <h2>The issue:</h2>
+<p>The housing inequality crisis refers to the increasing disparity in the quality of housing in the U.S. Quality of housing includes multiple aspects, including crime rate, quality of local schools, the amenities and the amount of air pollution.</p>
+
+<h2>The solution:</h2>
+<p>Our team believes that an effective solution to this issue is subsidized housing. This solution involves the government providing rent supplements to impoverished households. To avoid potential misuses of state funding, the rent supplements should be given directly to landlords. For instance, a person with an extensive history of drug abuse might spend rent supplements on drugs, and giving rent supplements directly to the landlord prevents this from occurring. </p>
+
+<h2>How the housing subsidy calculator contributes to the solution:</h2>
+<p>A key challenge in providing subsidies is determining the correct amount of subsidy to allocate to each household. To address this challenge, our team developed a housing subsidy calculator, which allows individuals to check the amount of subsidy that they should receive to guarantee a reasonable level of housing quality. </p>
+
+<h2>How the housing subsidy calculator works:</h2>
+<p>The calculator takes five inputs from the user:
+<li>1.	Monthly wage</li>
+<li>2.	Monthly rent</li>
+<li>3.	Zip code</li>
+<li>4.	Number of dependents</li>
+<li>5.	Marital status</li>
+Using the wage and rent, the calculator computes a ratio that represents the proportion of an individual’s income that’s spent on housing. This ratio is then compared with the “ideal” ratio of 1/3, and the difference between these two ratios suggests the amount of subsidy that’s required. 
+</p>
+
+
+<h1>Instructions:</h1>
+<p>
+<li>1.	Type in your pre-tax monthly wage in the first input box below </li>
+[note: your monthly wage must be below $4,000 to qualify for housing subsidy]
+
+<li>2.	Type in your monthly rent in the second box</li>
+
+<li>3.	Type in your zip code; this will be used to factor the crime rate and mental health index into our calculation</li>
+
+<li>4.	Type in the number of your dependents, including the number of your children and parents that you need to provide financial support for</li>
+
+<li>5.	Select your marital status, click on the “submit” button if you’re unmarried</li>
+
+<li>6.	If you’re married or divorced, enter the income of or financial support that you receive from the other person</li>
+
+See the formula that this calculator uses here.
+</p>
+
             <form className="ui form" onSubmit={this.onSubmit} style={this.getFormStyle()}>
               <div className="field" >
                 <div style={this.getFieldStyle()}>
@@ -67,43 +106,7 @@ export class finanaceForm extends Component {
               <div>
               <h1>The housing subsidy calculator:</h1>
 
-<h2>The issue:</h2>
-<p>The housing inequality crisis refers to the increasing disparity in the quality of housing in the U.S. Quality of housing includes multiple aspects, including crime rate, quality of local schools, the amenities and the amount of air pollution.</p>
 
-<h2>The solution:</h2>
-<p>Our team believes that an effective solution to this issue is subsidized housing. This solution involves the government providing rent supplements to impoverished households. To avoid potential misuses of state funding, the rent supplements should be given directly to landlords. For instance, a person with an extensive history of drug abuse might spend rent supplements on drugs, and giving rent supplements directly to the landlord prevents this from occurring. </p>
-
-<h2>How the housing subsidy calculator contributes to the solution:</h2>
-<p>A key challenge in providing subsidies is determining the correct amount of subsidy to allocate to each household. To address this challenge, our team developed a housing subsidy calculator, which allows individuals to check the amount of subsidy that they should receive to guarantee a reasonable level of housing quality. </p>
-
-<h2>How the housing subsidy calculator works:</h2>
-<p>The calculator takes five inputs from the user:
-1.	Monthly wage
-2.	Monthly rent
-3.	Zip code
-4.	Number of dependents
-5.	Marital status
-Using the wage and rent, the calculator computes a ratio that represents the proportion of an individual’s income that’s spent on housing. This ratio is then compared with the “ideal” ratio of 1/3, and the difference between these two ratios suggests the amount of subsidy that’s required. 
-</p>
-
-
-<h1>Instructions:</h1>
-<p>
-1.	Type in your pre-tax monthly wage in the first input box below 
-[note: your monthly wage must be below $4,000 to qualify for housing subsidy]
-
-2.	Type in your monthly rent in the second box
-
-3.	Type in your zip code; this will be used to factor the crime rate and mental health index into our calculation
-
-4.	Type in the number of your dependents, including the number of your children and parents that you need to provide financial support for
-
-5.	Select your marital status, click on the “submit” button if you’re unmarried
-
-6.	If you’re married or divorced, enter the income of or financial support that you receive from the other person
-
-See the formula that this calculator uses here.
-</p>
 
 
               </div>
